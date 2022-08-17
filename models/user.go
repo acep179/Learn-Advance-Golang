56d1cffd632c5b8091f8a38293b47927ba8d45fb
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-08-17 23:03:13 +0700

    Menambahkan komentar berupa catatan

--- models/user.go
+++ models/user.go
@@ -3,15 +3,19 @@ package models
 import "time"
 
 type User struct {
-	ID        int                   `json:"id"`
-	Name      string                `json:"name" gorm:"type: varchar(255)"`
-	Email     string                `json:"email" gorm:"type: varchar(255)"`
-	Password  string                `json:"-" gorm:"type: varchar(255)"`
-	Profile   ProfileResponse       `json:"profile"`
-	Products  []ProductUserResponse `json:"products"`
-	CreatedAt time.Time             `json:"-"`
-	UpdatedAt time.Time             `json:"-"`
+	ID       int                   `json:"id"`
+	Name     string                `json:"name" gorm:"type: varchar(255)"`
+	Email    string                `json:"email" gorm:"type: varchar(255)"`
+	Password string                `json:"-" gorm:"type: varchar(255)"`
+	Profile  ProfileResponse       `json:"profile"`
+	Products []ProductUserResponse `json:"products"`
+	//ctt Perlu diperhatikan bahwa sekarang ada propery Products yang akan berelasi ke ProductUserResponse
+	//ctt Terlihat juga ada sedikit perbedaan antara Pofile dan Products di atas. Yaitu, pada Products untuk perelasiannya menggunakan [] alias slice karena case-nya adalah one to many yang artinya satu User bisa mempunyai banyak Products. Itulah sebabnya kita menggunakan slice agar data Products dapat ditampung, bukan hanya satu akan tetapi banyak data Products.
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
+
+//todo Sekarang, kita lakukan perubahan pada repository users
 
 type UsersProfileResponse struct {
 	ID   int    `json:"id"`
--- repositories/users.go
+++ repositories/users.go
@@ -20,14 +20,22 @@ func RepositoryUser(db *gorm.DB) *repository {
 
 func (r *repository) FindUsers() ([]models.User, error) {
 	var users []models.User
-	// Using Preload("profile") to find data with relation to profile and Preload("Products") for relation to Products here ...
+
+	//todo Using Preload("profile") to find data with relation to profile and Preload("Products") for relation to Products here ...
+	err := r.db.Preload("Profile").Preload("Products").Find(&users).Error // add this code
+
+	//ctt Kita cukup menambahkan satu Preload("") lagi untuk menampilkan data Products-nya
+	//ctt Kita tidak perlu lagi menambahkan satu Find lagi di bawahnya, cukup tambahkan atau sisipkan satu Preload lagi untuk Products
+	//ctt Karena, jika kita Debug() maka sebenarnya GORM nya itu langsung menjalankan tiga perintah query untuk masing-masing Method
 
 	return users, err
 }
 
 func (r *repository) GetUser(ID int) (models.User, error) {
 	var user models.User
-	// Using Preload("profile") to find data with relation to profile and Preload("Products") for relation to Products here ...
+
+	//todo Using Preload("profile") to find data with relation to profile and Preload("Products") for relation to Products here ...
+	err := r.db.Preload("Profile").Preload("Products").First(&user, ID).Error // add this code
 
 	return user, err
 }
