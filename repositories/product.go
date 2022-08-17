ea0b6da71c8ec68afd016cae6a79dcf1e5dc7c5a
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-08-17 23:27:54 +0700

    Menambahkan komentar berupa catatan

--- repositories/product.go
+++ repositories/product.go
@@ -18,14 +18,21 @@ func RepositoryProduct(db *gorm.DB) *repository {
 
 func (r *repository) FindProducts() ([]models.Product, error) {
 	var products []models.Product
-	// Using Preload("User") to find data with relation to User and Preload("Category") for relation to Category here ...
+	//todo Using Preload("User") to find data with relation to User and Preload("Category") for relation to Category here ...
+	err := r.db.Preload("User").Preload("Category").Find(&products).Error
 
 	return products, err
 }
 
 func (r *repository) GetProduct(ID int) (models.Product, error) {
 	var product models.Product
-	// Using Preload("User") to find data with relation to User and Preload("Category") for relation to Category here ...
+	//todo Using Preload("User") to find data with relation to User and Preload("Category") for relation to Category here ...
+	err := r.db.Preload("User").Preload("Category").First(&product).Error
+
+	//ctt Seperti biasa, cukup tambahkan Preload-nya saja, yang penting kita paham mengenai cara merelasikannya pada models-nya yaitu pada tahap preparation-nya
+
+	//ctt Tetapi ada satu permasalahan yaitu ketika kita meng-create product dan mengisikan CategoryID-nya. Maka yang akan ter-update hanyalah CategoryID pada Products tersebut. Sedangkan, pada tabel jembatannya yaitu product_category hal tersebut tidaklah ter-handle
+	//ctt Maka dari itu, kita perlu meng-handle-nya di handler CreateProduct
 
 	return product, err
 }
